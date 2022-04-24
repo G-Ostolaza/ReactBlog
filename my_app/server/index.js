baaec0d841282e4a21a9cfe002d3a91ac6b245fb
Gabriel Ostolaza <Gabe.Osto@Gmail.com>
2022-04-24 14:27:38 -0700

    updated file

--- my_app/server/index.js
+++ my_app/server/index.js
@@ -8,8 +8,12 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+import postRoutes from './routes/post.js';
+
 const CONNECTION_URL= "mongodb+srv://G-Ostolaza:Iver$on03@cluster0.esdwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 4000;
+
+app.use("/posts",postRoutes);
 
 mongoose.connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
